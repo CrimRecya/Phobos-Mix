d1cda6171fce116c5195e64ba6c83270ac6b5a80
CrimRecya <335958461@qq.com>
2025-02-24 20:31:11 +0800

    功能说明

--- src/Ext/Sidebar/Hooks.cpp
+++ src/Ext/Sidebar/Hooks.cpp
@@ -107,16 +107,6 @@ DEFINE_HOOK(0x72FCB5, InitSideRectangles_CenterBackground, 0x5)
 	return 0;
 }
 
-#pragma region MarkRedraw
-
-DEFINE_HOOK(0x4F92DD, HouseClass_Update_RedrawSidebarWhenRecheckTechTree, 0x5)
-{
-	SidebarClass::Instance->SidebarBackgroundNeedsRedraw = true;
-	return 0;
-}
-
-#pragma endregion
-
 #pragma region DrawGreyCameoExtraCover
 
 DEFINE_HOOK(0x6A9BC5, StripClass_Draw_DrawGreyCameoExtraCover, 0x6)
