0960bd62e428c502f3e51e3c305697813bd67201
CrimRecya <335958461@qq.com>
2025-03-23 21:17:58 +0800

    格子拓展

---
+++ src/Ext/Cell/Body.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <CellClass.h>
+
+#include <Utilities/Container.h>
+#include <Utilities/Constructs.h>
+#include <Utilities/Template.h>
+
+class CellExt
+{
+public:
+	using base_type = CellClass;
+
+	static constexpr DWORD Canary = 0x13371337;
+	static constexpr size_t ExtPointerOffset = 0x144;
+
+	class ExtData final : public Extension<CellClass>
+	{
+	public:
+
+		ExtData(CellClass* OwnerObject) : Extension<CellClass>(OwnerObject)
+
+		{ }
+
+		virtual ~ExtData() = default;
+
+		virtual void InvalidatePointer(void* ptr, bool removed) override;
+
+		virtual void LoadFromStream(PhobosStreamReader& Stm) override;
+		virtual void SaveToStream(PhobosStreamWriter& Stm) override;
+
+	private:
+		template <typename T>
+		void Serialize(T& Stm);
+	};
+
+	class ExtContainer final : public Container<CellExt>
+	{
+	public:
+		ExtContainer();
+		~ExtContainer();
+
+		virtual bool InvalidateExtDataIgnorable(void* const ptr) const override;
+	};
+
+	static ExtContainer ExtMap;
+};
