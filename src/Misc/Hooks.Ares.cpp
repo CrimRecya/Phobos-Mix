c94c1683e2df975478e9dc48afb8765c9b180593
航味麻酱 <93972760+TaranDahl@users.noreply.github.com>
2025-04-04 00:06:58 +0800

    impl

--- src/Misc/Hooks.Ares.cpp
+++ src/Misc/Hooks.Ares.cpp
@@ -6,6 +6,7 @@
 #include <Utilities/Helpers.Alex.h>
 
 #include <Ext/Sidebar/Body.h>
+#include <Ext/Techno/Body.h>
 
 // In vanilla YR, game destroys building animations directly by calling constructor.
 // Ares changed this to call UnInit() which has a consequence of doing pointer invalidation on the AnimClass pointer.
@@ -43,6 +44,11 @@ void __fastcall LetGo(TemporalClass* pTemporal)
 	pTemporal->LetGo();
 }
 
+void __stdcall ConvertToType(TechnoClass* pThis,TechnoTypeClass* pToType)
+{
+	TechnoExt::ConvertToType(pThis, pToType);
+}
+
 void Apply_Ares3_0_Patches()
 {
 	// Abductor fix:
@@ -64,6 +70,12 @@ void Apply_Ares3_0_Patches()
 
 	// Replace the TemporalClass::Detach call by LetGo in convert function:
 	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x436DA, &LetGo);
+	
+	// Use new convert
+	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x39DAE, &ConvertToType);
+	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x46C6D, &ConvertToType);
+	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x4B397, &ConvertToType);
+	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x4C099, &ConvertToType);
 }
 
 void Apply_Ares3_0p1_Patches()
@@ -89,4 +101,10 @@ void Apply_Ares3_0p1_Patches()
 
 	// Replace the TemporalClass::Detach call by LetGo in convert function:
 	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x441BA, &LetGo);
+
+	// Use new convert
+	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x3A82E, &ConvertToType);
+	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x4780D, &ConvertToType);
+	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x4BFF7, &ConvertToType);
+	Patch::Apply_CALL(AresHelper::AresBaseAddress + 0x4CCF9, &ConvertToType);
 }
