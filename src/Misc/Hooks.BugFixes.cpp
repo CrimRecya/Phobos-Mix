cdaf09437b29cb55775fcff14c760579c3855a8c
CrimRecya <335958461@qq.com>
2025-04-10 22:07:30 +0800

    继续修阴影

--- src/Misc/Hooks.BugFixes.cpp
+++ src/Misc/Hooks.BugFixes.cpp
@@ -1862,7 +1862,9 @@ DEFINE_HOOK(0x710352, FootClass_ImbueLocomotor_ResetUnloadingHarvester, 0x7)
 	return 0;
 }
 
-DEFINE_HOOK(0x73C43F, UnitClass_DrawAsVXL_Shadow_IsLocomotorFix, 0x6)
+DEFINE_JUMP(LJMP, 0x73C41B, 0x73C431)
+
+DEFINE_HOOK(0x73C43F, UnitClass_DrawAsVXL_Shadow_IsLocomotorFix2, 0x6)
 {
 	enum { SkipGameCode = 0x73C445 };
 
