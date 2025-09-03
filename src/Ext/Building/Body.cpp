811a8431886db62026fc9e342fee8485d0a5928c
CrimRecya <335958461@qq.com>
2025-09-03 22:00:21 +0800

    优化防卡重工

--- src/Ext/Building/Body.cpp
+++ src/Ext/Building/Body.cpp
@@ -445,7 +445,12 @@ void BuildingExt::KickOutStuckUnits(BuildingClass* pThis)
 		{
 			if (const auto pUnit = abstract_cast<UnitClass*, true>(pObject))
 			{
-				if (pThis->Owner != pUnit->Owner || pUnit->Locomotor->Destination() != CoordStruct::Empty)
+				if (pThis->Owner != pUnit->Owner)
+					continue;
+
+				const auto pLocoDest = pUnit->Locomotor->Destination();
+
+				if (pLocoDest != CoordStruct::Empty && pLocoDest != pUnit->Location)
 					continue;
 
 				if (TechnoExt::IsAttached(pUnit))
