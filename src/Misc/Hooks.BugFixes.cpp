CrimRecya/Phobos-Mix

commit fc12197d73a9e73247e2cf9ff032923af7b133d4

Fix remaining issues with unit sell bugfix
- Change the EventClass hook to verify if object is actually sellable
- Add changelog entry and reword the documentation
- Add credits

src/Misc/Hooks.BugFixes.cpp
@@ -2950,6 +2950,8 @@ DEFINE_HOOK(0x65DE82, TeamTypeClass_CreateTeamMembers_Veterancy, 0x6)
 	return pTechnoType->Trainable ? 0 : SkipVeterancy;
 }
 
+#pragma region SellUnitFix
+
 // Disallow sell action on wall overlays if mouse cursor is hovering on another object.
 DEFINE_HOOK(0x692AD6, ScrollClass_ChooseAction_SellWall, 0x6)
 {
@@ -2957,24 +2959,50 @@ DEFINE_HOOK(0x692AD6, ScrollClass_ChooseAction_SellWall, 0x6)
 
 	GET(ObjectClass*, pObject, ESI);
 
-	if (pObject)
-		return NoSell;
-
-	return 0;
+	return pObject ? NoSell : 0;
 }
 
-// Disallow sell action on wall overlays at event/network level if there's an object on the cell.
-DEFINE_HOOK(0x4C6FCE, HouseClass_SellOverlay_ObjectCheck, 0x5)
+static bool inline CanBeSold(TechnoClass* pTechno, AbstractType rtti)
 {
-	enum { SkipSellOverlay = 0x4C6FD3 };
+	if (rtti == AbstractType::Building)
+		return !static_cast<BuildingClass*>(pTechno)->Type->Unsellable;
 
-	GET(EventClass*, pEvent, ESI);
+	if (rtti == AbstractType::Unit || rtti == AbstractType::Aircraft)
+	{
+		auto const pTypeExt = TechnoExt::ExtMap.Find(pTechno)->TypeExtData;
 
-	if (MapClass::Instance.GetCellAt(pEvent->SellCell.Location)->GetContent())
-		return SkipSellOverlay;
+		if (!pTypeExt->Unsellable.Get(RulesExt::Global()->UnitsUnsellable))
+			return false;
 
-	return 0;
+		auto const pCell = MapClass::Instance.GetCellAt(pTechno->GetCenterCoords());
+
+		if (auto const pBuilding = pCell->GetBuilding())
+		{
+			auto const pType = pBuilding->Type;
+
+			if (BuildingTypeExt::ExtMap.Find(pType)->UnitSell.Get(pType->UnitRepair))
+				return true;
+		}
+	}
+
+	return false;
 }
+
+// Verify if object can be sold at event level.
+DEFINE_HOOK(0x4C6F55, EventClass_Execute_Sell, 0x5)
+{
+	enum { SkipGameCode = 0x4C6FA8 };
+
+	GET(TechnoClass*, pTechno, EDI);
+	GET(AbstractType, rtti, EAX);
+
+	if (CanBeSold(pTechno, rtti))
+		pTechno->Sell(-1);
+
+	return SkipGameCode;
+}
+
+#pragma endregion
 
 // Fixed the issue where non-repairer units needed sensors to attack cloaked friendly units.
 DEFINE_JUMP(LJMP, 0x6FC278, 0x6FC289);

src/Ext/BuildingType/Body.cpp
@@ -272,6 +272,9 @@ void BuildingTypeExt::ExtData::LoadFromINIFile(CCINIClass* const pINI)
 	// Ares 0.A
 	this->RubbleIntact.Read(exINI, pSection, "Rubble.Intact");
 	this->RubbleIntactRemove.Read(exINI, pSection, "Rubble.Intact.Remove");
+
+	// Ares 3.0
+	this->UnitSell.Read(exINI, pSection, "UnitSell");
 }
 
 void BuildingTypeExt::ExtData::CompleteInitialization()
@@ -360,6 +363,9 @@ void BuildingTypeExt::ExtData::Serialize(T& Stm)
 		// Ares 0.A
 		.Process(this->RubbleIntact)
 		.Process(this->RubbleIntactRemove)
+
+		// Ares 3.0
+		.Process(this->UnitSell)
 		;
 }
 

src/Ext/BuildingType/Body.h
@@ -110,6 +110,9 @@ public:
 		Valueable<BuildingTypeClass*> RubbleIntact;
 		Valueable<bool> RubbleIntactRemove;
 
+		// Ares 3.0
+		Nullable<bool> UnitSell;
+
 		ExtData(BuildingTypeClass* OwnerObject) : Extension<BuildingTypeClass>(OwnerObject)
 			, PowersUp_Owner { AffectedHouse::Owner }
 			, PowersUp_Buildings {}
@@ -185,6 +188,9 @@ public:
 			// Ares 0.A
 			, RubbleIntact { nullptr }
 			, RubbleIntactRemove { false }
+
+			// Ares 3.0
+			, UnitSell {}
 		{ }
 
 		// Ares 0.A functions

src/Ext/Rules/Body.cpp
@@ -368,6 +368,8 @@ void RulesExt::ExtData::LoadBeforeTypeData(RulesClass* pThis, CCINIClass* pINI)
 
 	this->PenetratesTransport_Level.Read(exINI, GameStrings::CombatDamage, "PenetratesTransport.Level");
 
+	this->UnitsUnsellable.Read(exINI, GameStrings::General, "UnitsUnsellable");
+
 	// Section AITargetTypes
 	int itemsCount = pINI->GetKeyCount("AITargetTypes");
 	for (int i = 0; i < itemsCount; ++i)
@@ -670,6 +672,7 @@ void RulesExt::ExtData::Serialize(T& Stm)
 		.Process(this->DefaultToGuardArea)
 		.Process(this->CylinderRangefinding)
 		.Process(this->PenetratesTransport_Level)
+		.Process(this->UnitsUnsellable)
 		;
 }
 

src/Ext/Rules/Body.h
@@ -318,6 +318,8 @@ public:
 
 		Valueable<int> PenetratesTransport_Level;
 
+		Valueable<bool> UnitsUnsellable;
+
 		ExtData(RulesClass* OwnerObject) : Extension<RulesClass>(OwnerObject)
 			, Storage_TiberiumIndex { -1 }
 			, HarvesterDumpAmount { 0.0f }
@@ -579,6 +581,8 @@ public:
 			, CylinderRangefinding { false }
 
 			, PenetratesTransport_Level { 10 }
+
+			, UnitsUnsellable { false }
 		{ }
 
 		virtual ~ExtData() = default;

src/Ext/TechnoType/Body.cpp
@@ -1177,6 +1177,9 @@ void TechnoTypeExt::ExtData::LoadFromINIFile(CCINIClass* const pINI)
 	// Ares 2.0
 	this->Passengers_BySize.Read(exINI, pSection, "Passengers.BySize");
 
+	// Ares 3.0
+	this->Unsellable.Read(exINI, pSection, "Unsellable");
+
 	if (pThis->Gunner)
 	{
 		size_t weaponCount = pThis->WeaponCount;
@@ -1871,6 +1874,8 @@ void TechnoTypeExt::ExtData::Serialize(T& Stm)
 		.Process(this->PenetratesTransport_DamageMultiplier)
 
 		.Process(this->JumpjetClimbIgnoreBuilding)
+
+		.Process(this->Unsellable)
 		;
 }
 void TechnoTypeExt::ExtData::LoadFromStream(PhobosStreamReader& Stm)

src/Ext/TechnoType/Body.h
@@ -485,6 +485,8 @@ public:
 
 		Nullable<bool> JumpjetClimbIgnoreBuilding;
 
+		Nullable<bool> Unsellable; // Ares 3.0
+
 		ExtData(TechnoTypeClass* OwnerObject) : Extension<TechnoTypeClass>(OwnerObject)
 			, HealthBar_Hide { false }
 			, HealthBar_HidePips { false }
@@ -923,6 +925,8 @@ public:
 			, PenetratesTransport_DamageMultiplier { 1.0 }
 
 			, JumpjetClimbIgnoreBuilding {}
+
+			, Unsellable {}
 		{ }
 
 		virtual ~ExtData() = default;

commit 651bff6e135d7113b2ebe00fe81ce2b954896cd1

Fix the Unsellable judgment error introduced by fc1219 (#2183)
Fix the bug introduced by commit
fc12197d73a9e73247e2cf9ff032923af7b133d4 where the `Unsellable` [value
incorrectly
corresponded](https://github.com/Phobos-developers/Phobos/commit/fc12197d73a9e73247e2cf9ff032923af7b133d4#diff-5e24fcccc37dd8fb07ae7b1fb3a409dfd21b68c5a0aa8782f79a4007d2afda62R2965-R2974)
to whether it could be sold.

docs/locale/zh_CN/LC_MESSAGES/Whats-New.po
@@ -2516,8 +2516,8 @@ msgstr "0.4.0.3"
 
 msgid ""
 "Vehicles overlapping `Wall=true` OverlayTypes no longer display sell "
-"cursor and cannot be sold (by CnCRAZER & Starkku)"
-msgstr "图像与 `Wall=true` 的覆盖物重叠的载具上不再会显示出售光标也不再能被出售（by RAZER & Starkku）"
+"cursor and cannot be sold (by CnCRAZER, Starkku, Noble_Fish)"
+msgstr "图像与 `Wall=true` 的覆盖物重叠的载具上不再会显示出售光标也不再能被出售（by RAZER、Starkku、Noble_Fish）"
 
 msgid ""
 "Fixed a desync due to an inconsistent shroud state caused by "

src/Misc/Hooks.BugFixes.cpp
@@ -3078,7 +3078,7 @@ static bool inline CanBeSold(TechnoClass* pTechno, AbstractType rtti)
 	{
 		auto const pTypeExt = TechnoExt::ExtMap.Find(pTechno)->TypeExtData;
 
-		if (!pTypeExt->Unsellable.Get(RulesExt::Global()->UnitsUnsellable))
+		if (pTypeExt->Unsellable.Get(RulesExt::Global()->UnitsUnsellable))
 			return false;
 
 		auto const pCell = MapClass::Instance.GetCellAt(pTechno->GetCenterCoords());